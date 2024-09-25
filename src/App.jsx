db9d3aa0688074f350d39fad9f5512cc9e9fcd40
Afomia Tadesse <afomiat79@gmail.com>
2024-09-25 15:37:12 -0700

    started main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import Sidebar from "./components/sidebar/sidebar"
+import Main from "./components/Main/Main"
 const App = () => {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   )
 }
